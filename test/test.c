9adb96f8678f9c17506a06e1e459f041ba05cedf
Robert Rostohar <robert.rostohar@arm.com>
2025-09-01 09:57:37 +0200

    Add test files

---
+++ test/test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void test_unused(void) {
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+  printf("Unused\n"); printf("Unused\n"); printf("Unused\n"); printf("Unused\n");
+}
+
+void test(void) {
+  printf("Test\n");
+}
